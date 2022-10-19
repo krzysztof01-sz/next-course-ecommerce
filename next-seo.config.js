3d9771e96750d4f93a2dd8b86429d423b1682823
Krzysztof <krzysztofs012@outlook.com>
2022-10-19 22:13:15 +0200

    feat: define default seo tags

---
+++ next-seo.config.js
@@ -0,0 +1,10 @@
+const settings = {
+  title: "Next e-commerce app",
+  description: "My e-commerce next project",
+  openGraph: {
+    title: "Next e-commerce app",
+    description: "My e-commerce next project",
+  },
+};
+
+export default settings;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Layout } from "../components/Layout";
+import seo from "../next-seo.config";
+import { DefaultSeo } from "next-seo";
 
 const client = new QueryClient();
 
@@ -9,6 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={client}>
       <Layout>
+        <DefaultSeo {...seo} />
         <Component {...pageProps} />
       </Layout>
     </QueryClientProvider>
