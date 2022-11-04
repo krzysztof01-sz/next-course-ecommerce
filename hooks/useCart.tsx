361625508922fba894395f5c6ab61490c3c6304d
Krzysztof <krzysztofs012@outlook.com>
2022-11-04 22:32:28 +0100

    refactor: separate logic to useCart hook

---
+++ hooks/useCart.tsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+
+interface CartProduct {
+  id: number;
+  title: string;
+  price: number;
+  count: number;
+}
+
+interface Cart {
+  products: CartProduct[];
+  addProduct: (product: Omit<CartProduct, "count">) => void;
+  deleteProduct: (id: CartProduct["id"]) => void;
+}
+
+const defaultContext: Cart = {
+  products: [],
+  addProduct: () => {},
+  deleteProduct: () => [],
+};
+
+export const useCart = (): Cart => {
+  const [cart, setCart] = useState<Cart>();
+
+  useEffect(() => {
+    const localStorageCart = localStorage.getItem("NEXT_SHOPPING_CART");
+    if (localStorageCart) {
+      const cart = JSON.parse(localStorageCart);
+      if (cart) {
+        return setCart({ ...defaultContext, products: cart });
+      }
+    }
+    return setCart(defaultContext);
+  }, []);
+
+  useEffect(() => {
+    if (cart && cart.products) {
+      localStorage.setItem("NEXT_SHOPPING_CART", JSON.stringify(cart.products));
+    }
+  }, [cart]);
+
+  const addProduct = (product: Omit<CartProduct, "count">) => {
+    if (!cart) return defaultContext;
+
+    const existingProduct = cart.products.find(
+      (item) => item.id === product.id
+    );
+
+    if (existingProduct) {
+      const newProducts = cart.products.map((item) => {
+        if (item.id === product.id) {
+          return { ...item, count: item.count + 1 };
+        }
+        return { ...item };
+      });
+
+      return setCart((prevCart) => ({
+        ...(prevCart as Cart),
+        products: newProducts,
+      }));
+    }
+
+    return setCart((prevCart) => ({
+      ...(prevCart as Cart),
+      products: [...cart.products, { ...product, count: 1 }],
+    }));
+  };
+
+  const deleteProduct = (id: number) => {
+    if (!cart) return defaultContext;
+
+    const newProducts = cart.products
+      .map((item) => {
+        if (item.id === id) {
+          return { ...item, count: item.count - 1 };
+        }
+        return { ...item };
+      })
+      .filter(({ count }) => count > 0);
+
+    return setCart((prevCart) => ({
+      ...(prevCart as Cart),
+      products: newProducts,
+    }));
+  };
+
+  if (!cart) return defaultContext;
+
+  return { addProduct, deleteProduct, products: cart.products };
+};
