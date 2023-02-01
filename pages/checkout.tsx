b88cdedd124e5e5a60946ee10e3042cc5b0ca1a9
Krzysztof <krzysztofs012@outlook.com>
2023-02-01 22:21:56 +0100

    feat: add basic chekcout form

---
+++ pages/checkout.tsx
@@ -0,0 +1,12 @@
+import { CheckoutForm } from "../components/CheckoutForm";
+import { Main } from "../components/Main";
+
+const CheckoutPage = () => {
+  return (
+    <Main>
+      <CheckoutForm />
+    </Main>
+  );
+};
+
+export default CheckoutPage;
